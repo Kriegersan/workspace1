4b40ef4d0a4891916b942ddbe57436ee864bb027
Nelson <keith.nelson@tylertech.com>
2019-09-06 13:59:49 -0400

    workspace1 v1.5

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello WOrld!</h1>'
+#@app.route('/')
+#def hello():
+#   return '<h1>Hello WOrld!</h1>'
+
+@app.route('/<name>')
+def hello_name(name=None):
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
